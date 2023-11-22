e2a07e2fb340806cfc949bbd4f280aa9fc4784df
Christian Rojas <68656093+ChrissRojas@users.noreply.github.com>
2023-11-22 14:49:39 +0000

    update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
 from flask import render_template, flash, redirect, url_for, request, session
 import spotipy
-from app import app
+from app import app,sp_auth
+
+scopes = sp_auth.scopes
+spfy_oauth = sp_auth.SP_HANDLER(scopes)
 
 @app.route('/')
 @app.route('/index')
@@ -12,6 +15,10 @@ def index():
 @app.route('/login', methods=['POST','GET'])
 def login():
     return redirect('/index')
+
+@app.route('/api_callback')
+def callback():
+    request.args.get('code')
 
 @app.route('/logout', methods=['POST','GET'])
 def logout():

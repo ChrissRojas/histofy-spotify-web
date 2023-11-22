c5297c652c27d9ec7588b4f6ba269f3dd4d53c42
Christian Rojas <68656093+ChrissRojas@users.noreply.github.com>
2023-11-22 16:20:21 +0000

    implement login and logout

--- app/routes.py
+++ app/routes.py
@@ -8,20 +8,26 @@ spfy_oauth = sp_auth.SP_HANDLER(scopes)
 @app.route('/')
 @app.route('/index')
 def index():
-
-    user = {}
+    user={}
     return render_template('index.html', login=user)
 
-@app.route('/login', methods=['POST','GET'])
+@app.route('/login', methods=['GET'])
 def login():
-    return redirect('/index')
+    return redirect(spfy_oauth.get_auth_url())
 
 @app.route('/api_callback')
 def callback():
     code = request.args.get('code')
-    spfy_oauth.callback_token(code)
-    session.clear
+    session.clear()
+    tk_info = spfy_oauth.callback_token(code)
+    session['token_info'] = tk_info
+    # spotify=spotipy.Spotify(auth_manager=spfy_oauth.sp)
+    # print(spotify.me())
+    print(session['token_info'])
+    return redirect('/index')
 
 @app.route('/logout', methods=['POST','GET'])
 def logout():
+    session.pop('token_info', None)
     return redirect('/index')
+
